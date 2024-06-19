80f96dce7c7e9aa58b55b35a746041f0022d153c
JesusJK <camilojesus19@gmail.com>
2024-06-18 23:30:29 -0500

    mejor solucion con iconos

---
+++ src/components/icons/alert.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'RiAlertFill',
+});
+</script>
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+		<path fill="currentColor" d="m12.866 3l9.526 16.5a1 1 0 0 1-.866 1.5H2.474a1 1 0 0 1-.866-1.5L11.134 3a1 1 0 0 1 1.732 0M11 16v2h2v-2zm0-7v5h2V9z" />
+	</svg>
+</template>
--- src/components/vue/Correos.vue
+++ src/components/vue/Correos.vue
@@ -1,46 +1,44 @@
 <script setup>
 import { ref } from 'vue';
 
+
 const emails = ref([
-    { title: "Correo 1", text: "Texto del correo 1", time: "Hace 1 hora", estado: false, negrilla: 'font-black', borde: 'border-gray-400', snd:true },
-    { title: "Correo 2", text: "Texto del correo 2", time: "Hace 1 hora", estado: false, negrilla: 'font-black', borde: 'border-gray-400', snd:true },
-    { title: "Correo 3", text: "Texto del correo 3", time: "Hace 2 hora", estado: false, negrilla: 'font-black', borde: 'border-gray-400', snd:true },
-    { title: "Correo 4", text: "Texto del correo 4", time: "Hace 3 hora", estado: false, negrilla: 'font-black', borde: 'border-gray-400', snd:true },
+    { title: "Correo 1", text: "Texto del correo 1", time: "Hace 1 hora", estado: false, snd:true },
+    { title: "Correo 2", text: "Texto del correo 2", time: "Hace 1 hora", estado: false, snd:true },
+    { title: "Correo 3", text: "Texto del correo 3", time: "Hace 2 hora", estado: false, snd:true },
+    { title: "Correo 4", text: "Texto del correo 4", time: "Hace 3 hora", estado: false, snd:true },
 ])
 
 function select(index) {
 emails.value[index].estado=!emails.value[index].estado
-if (emails.value[index].estado == true) {
-    emails.value[index].borde = 'border-blue-500'
-} else if(emails.value[index].estado == false){
-    emails.value[index].borde = 'border-gray-400'
-}
 
 }
 
 function marcar(index) {
     emails.value[index].snd=false
-    emails.value[index].negrilla='font-serif'
 }
 
 </script>
 
 <template>
+
     <div class="m-0 p-0 border-black border ">
         <h1 class="text-lg text-center">Correos</h1>
 
-        <div v-for="(email, index) in emails" :key="index" :class="email.borde"
+        <div v-for="(email, index) in emails" :key="index" :class="{'border-blue-500':email.estado}"
             class="bg-white/60 border-2 px-2 m-2 p-2 w-[25%] h-max text-start mx-auto hover:border-blue-500 ">
 
             <div id="caja" class="flex" @click="select(index)">
-                <div class="p-2 m-2 items-center"></div>
+                <div class="p-2 m-2 items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+                </div>
                 <div class="flex-auto">
                     <div class="flex justify-between">
 
-                        <span :class="email.negrilla">{{ email.title }}</span>
+                        <span class="font-serif" :class="{'font-black':email.snd}">{{ email.title }}</span>
 
                         <button @click="marcar(index)" v-show="email.estado && email.snd"
-                            class=" text-center w-max text-xs text-gray-300">Marcar
+                            class=" text-center w-max text-xs text-gray-300" >Marcar
                             como leido</button>
 
                     </div>
